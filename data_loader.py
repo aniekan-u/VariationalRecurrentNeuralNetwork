3eac13fd33cc0a63ef0c93c1ac3a0c0e70a97d94
Aniekan Umoren <anixmc1@gmail.com>
2021-12-01 20:21:56 -0500

    implemented dataset

--- data_loader.py
+++ data_loader.py
@@ -11,25 +11,40 @@ from nlb_tools.nwb_interface import NWBDataset
 
 
 
-class NLB_Loader(data.Dataset):
-    def __init__(self, experiment, train, batchsize, resample_val, shuffle, seed_val, seq_len, neur_count):
+class NLB(data.Dataset):
+    def __init__(self, experiment, train, resample_val, shuffle, seed_val, seq_len, neur_count, seq_start_mode='all'):
 
         '''
         INPUT
         experiment     int from 1-4 chooses experiment (1: MC_Maze, 2: MC_RTT, 3: Area2_Bump, 4: DMFC_RSG)
         train          bool true -> train, false -> test
-        batchsize      int determines amount of trials at once
         resample_val   int determines factor of resampling,    1 returns original, once resampled need to redownload to get original (delete folder)
         shuffle        bool shuffles trials
         seq_len        int length of individual sequences,     0 for full length
         neur_count     int count of neurons per trial,         0 for full length
-
+        seq_start_mode str                                     ['all', 'unique']
+        
         OUTPUT
-        neuron_id        2d int-array of neuron ids in data
+        neuron_id      2d int-array of neuron ids in data
         trial_id       1d int-array of trial ids in data
         data           3d numpy array of trials x neurons x sequences
         '''
-
+        
+        # Instance variables
+        
+        assert experiment in [i+1 in range(4)], 'experiment must be in range 1-4'
+        self.experiment = experiment
+        self.train = train
+        self.seq_len = seq_len
+        self.neur_count = neur_count
+        assert seq_start_mode in {'all', 'unique'}, 'seq_start_mode must be in {all, unique}'
+        self.seq_start_mode = seq_start_mode
+        
+        self.dataset = None
+        self.trail_ids = None
+        self.neuron_ids = None
+        self.N_sequences = None
+        
         if experiment == 1:
             if not os.path.isdir("000128"):
                 print("Downloading data")
@@ -70,65 +85,64 @@ class NLB_Loader(data.Dataset):
             else:
                 dataset = NWBDataset("000129/sub-Indy", "*test", split_heldout=False)
 
-        else:
-            print("Experiment only 1-4")
-
 
         # Seed generator for consistent plots
         np.random.seed(seed_val)
-
 
         dataset.resample(resample_val)
 
         # Smooth spikes with 50 ms std Gaussian
         dataset.smooth_spk(50, name='smth_50')
 
-        dataset = dataset.make_trial_data()
+        self.dataset = dataset.make_trial_data()
         
-        trial_ids = np.unique(dataset['trial_id'])
-        eligible_ids = []
+        self.trial_ids = np.unique(self.dataset['trial_id'])
+        eligible_trials = {} # ID -> size
+        
+        # Only include trials of suffecient length
+        for ID in self.trial_ids:
+            trial_len = len(self.dataset[self.dataset['trial_id'] == ID])
+            if trial_len >= self.seq_len:
+                eligible_trials[ID] = trial_len
 
-        for ID in trial_ids:
-            if len(dataset[dataset['trial_id'] == ID]) >= seq_len:
-                eligible_ids.append(ID)
+        self.trial_ids = np.array(eligible_trails.keys())
+        if shuffle: np.random.shuffle(self.trial_ids)
 
-        eligible_ids = np.array(eligible_ids)
-
-        if shuffle: np.random.shuffle(eligible_ids)
-        trial_ids = eligible_ids
-
-        if shuffle: np.random.shuffle(trial_ids)
-
-        neuron_ids = np.array(dataset['spikes'].keys().tolist())
-        np.random.shuffle(neuron_ids)
+        cur_loc = 0
+        possible_starts = [] # list of (ID, time) tuples
+        if sel.seq_start_mode == 'all':
+            # allows for all possible sequences when all seq_len trials elems are from the same trial
+            for ID in self.trial_IDs:
+                lt = [(ID,t) for t in range(eligible_trials[ID] - self.seq_len)]
+                possible_starts.extend(lt)
+        
+        elif self.seq_start_mode == 'unique':
+            # creates sequences where each unique elem is in at most one sequence
+            for ID in self.trial_IDs: 
+                lt = [(ID,t) for t in range(0,eligible_trials[ID] - self.seq_len, self.seq_len)]
+                possible_starts.extend(lt)
+        
+        self.possible_starts = possible_starts
+        self.N_sequences = len(possible_start)
+        
+        self.neuron_ids = np.array(self.dataset['spikes'].keys().tolist())
+        np.random.shuffle(self.neuron_ids)
 
         if neur_count == 0:
-            neur_count = len(neuron_ids)
+            self.neur_count = len(self.neuron_ids)
 
-        neuron_ids = neuron_ids[0:neur_count]
-
+        self.neuron_ids = self.neuron_ids[0:neur_count]
 
         if seq_len == 0:
-            seq_len = len(dataset['spikes'][neuron_ids[0]])
+            self.seq_len = len(self.dataset['spikes'][self.neuron_ids[0]])
 
     def __getitem__(self, index):
 
-        data = np.zeros((batchsize, seq_len, neur_count))
+        trial_id, start = self.possible_starts[index]
+        trial_id = np.array([trial_id])
+        data = self.dataset[trial_id]['spikes'][self.neuron_ids][start:start + self.seq_len]
 
-        trial_ids = trial_ids[0:batchsize]
-
-        for i in range(batchsize):
-            trial_data = dataset[dataset['trial_id'] == trial_ids[i]]
-            trial_data = trial_data['spikes'][neuron_ids][0:seq_len]
-
-            data[i,:,:] = trial_data
-
-        return data, neuron_ids, trial_ids
+        return data, self.neuron_ids, trial_id
     
-
-if __name__ == "__main__":
-    data, neuron_ids, trial_ids = load_data(experiment=3, train=True, batchsize=100, resample_val=1, shuffle=False, seed_val=111, seq_len=300, neur_count=50)
-
-    print(data.shape)
-    print(neuron_ids.shape)
-    print(trial_ids.shape)
+    def __len__(self):
+        return self.N_sequences
